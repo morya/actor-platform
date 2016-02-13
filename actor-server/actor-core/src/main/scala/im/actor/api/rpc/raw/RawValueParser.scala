974064b28de336bafd18c1f6dd10f45b7b591060
rockjam <5min4eq.unity@gmail.com>
2016-02-13 03:51:00 +0300

    feat(serer): api array parsers improvements

--- actor-server/actor-core/src/main/scala/im/actor/api/rpc/raw/RawValueParser.scala
+++ actor-server/actor-core/src/main/scala/im/actor/api/rpc/raw/RawValueParser.scala
@@ -5,54 +5,55 @@ import im.actor.api.rpc.collections._
 import scala.util.Try
 
 trait RawValueParser[A] {
-  def apply(raw: ApiRawValue): Option[A]
+  def parse(raw: ApiRawValue): Option[A]
 }
 
 // target parsing type is ApiArrayValue
 // todo: add parser for ApiMapValue, to make Map[String, Any]. Not too usefull
 object RawValueParser {
+  def apply[T: RawValueParser]: RawValueParser[T] = implicitly[RawValueParser[T]]
 
-  def parse[A](raw: ApiRawValue)(implicit parser: RawValueParser[A]): Option[A] = parser(raw)
+  def parse[A: RawValueParser](raw: ApiRawValue): Option[A] = RawValueParser[A].parse(raw)
 
   import shapeless._
 
   implicit val booleanParser = new RawValueParser[Boolean] {
-    def apply(raw: ApiRawValue): Option[Boolean] = raw match {
+    def parse(raw: ApiRawValue): Option[Boolean] = raw match {
       case ApiStringValue(v) ⇒ Try(v.toBoolean).toOption
       case _                 ⇒ None
     }
   }
 
   implicit val stringParser = new RawValueParser[String] {
-    def apply(raw: ApiRawValue): Option[String] = raw match {
+    def parse(raw: ApiRawValue): Option[String] = raw match {
       case ApiStringValue(v) ⇒ Some(v)
       case _                 ⇒ None
     }
   }
 
   implicit val doubleParser = new RawValueParser[Double] {
-    def apply(raw: ApiRawValue): Option[Double] = raw match {
+    def parse(raw: ApiRawValue): Option[Double] = raw match {
       case ApiDoubleValue(v) ⇒ Some(v)
       case _                 ⇒ None
     }
   }
 
   implicit val intParser = new RawValueParser[Int] {
-    def apply(raw: ApiRawValue): Option[Int] = raw match {
+    def parse(raw: ApiRawValue): Option[Int] = raw match {
       case ApiInt32Value(v) ⇒ Some(v)
       case _                ⇒ None
     }
   }
 
   implicit val longParser = new RawValueParser[Long] {
-    def apply(raw: ApiRawValue): Option[Long] = raw match {
+    def parse(raw: ApiRawValue): Option[Long] = raw match {
       case ApiInt64Value(v) ⇒ Some(v)
       case _                ⇒ None
     }
   }
 
   implicit val hnilParser: RawValueParser[HNil] = new RawValueParser[HNil] {
-    def apply(raw: ApiRawValue): Option[HNil] = raw match {
+    def parse(raw: ApiRawValue): Option[HNil] = raw match {
       case ApiArrayValue(arr) if arr.isEmpty ⇒ Some(HNil)
       case _                                 ⇒ None
     }
@@ -60,13 +61,12 @@ object RawValueParser {
 
   // we know how to parse ApiRawValue to hcons only if ApiRawValue is Array
   implicit def hconsParser[H: RawValueParser, T <: HList: RawValueParser]: RawValueParser[H :: T] = new RawValueParser[H :: T] {
-    def apply(raw: ApiRawValue): Option[H :: T] = raw match {
+    def parse(raw: ApiRawValue): Option[H :: T] = raw match {
       case ApiArrayValue(arr) ⇒ arr match {
         case h +: t ⇒
           for {
-            // todo: intead of implicitly - provide apply method in object, that will get implicitly
-            head ← implicitly[RawValueParser[H]].apply(h)
-            tail ← implicitly[RawValueParser[T]].apply(ApiArrayValue(t))
+            head ← RawValueParser[H].parse(h)
+            tail ← RawValueParser[T].parse(ApiArrayValue(t))
           } yield head :: tail
       }
       case _ ⇒ None
@@ -74,7 +74,7 @@ object RawValueParser {
   }
 
   implicit def caseClassParser[A, R <: HList](implicit gen: Generic[A] { type Repr = R }, reprParser: RawValueParser[R]): RawValueParser[A] = new RawValueParser[A] {
-    def apply(raw: ApiRawValue): Option[A] = reprParser.apply(raw).map(gen.from)
+    def parse(raw: ApiRawValue): Option[A] = reprParser.parse(raw).map(gen.from)
   }
 
 }
